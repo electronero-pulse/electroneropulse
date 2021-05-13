04e415b1f6f3dc3bc5620f16e3a2401558d279ef
Interchained <evansmark.work@gmail.com>
2021-05-13 11:03:13 -0400

    HF 20 fork to solve alt chain

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -76,6 +76,7 @@
 #define MAINNET_HARDFORK_V17_HEIGHT ((uint64_t)(665500)) // MAINNET v17 hard fork
 #define MAINNET_HARDFORK_V18_HEIGHT ((uint64_t)(1124721)) // MAINNET v18 hard fork
 #define MAINNET_HARDFORK_V19_HEIGHT ((uint64_t)(1124950)) // MAINNET v19 hard fork
+#define MAINNET_HARDFORK_V20_HEIGHT ((uint64_t)(1667299)) // MAINNET v20 hard fork
 
 #define TESTNET_ELECTRONERO_HARDFORK ((uint64_t)(12746)) // Electronero TESTNET fork height
 #define TESTNET_HARDFORK_V1_HEIGHT ((uint64_t)(1)) // TESTNET v1 
@@ -167,6 +168,8 @@ static const struct {
   { 18, MAINNET_HARDFORK_V18_HEIGHT, 0, 1581655928 },
   // Version 19
   { 19, MAINNET_HARDFORK_V19_HEIGHT, 0, 1582955928 },
+  // Version 20
+  { 20, MAINNET_HARDFORK_V20_HEIGHT, 0, 1620978052 },
 };
 static const uint64_t mainnet_hard_fork_version_1_till = MAINNET_HARDFORK_V7_HEIGHT-1;
 
