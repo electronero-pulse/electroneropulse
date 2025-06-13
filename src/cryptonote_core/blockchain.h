7fa9ab693d3e73f90e910255f6857a80dc018649
⛓️INTERCHAINED ⚡Electronero Network ʕ•̫͡•ʔ-̫͡-ʕ•͓͡•ʔ-̫͡-ʕ•̫͡•ʔ-̫͡-ʔ <evansmark.work@gmail.com>
2025-06-12 22:17:44 -0400

    Update blockchain.h

--- src/cryptonote_core/blockchain.h
+++ src/cryptonote_core/blockchain.h
@@ -30,11 +30,11 @@
 
 #pragma once
 
-#include <boost/serialization/library_version_type.hpp>
 #include <boost/asio/io_service.hpp>
 #define BOOST_SERIALIZATION_VERSION_HPP
 #include <boost/serialization/version.hpp>
 #include <boost/serialization/serialization.hpp>
+#include <boost/serialization/library_version_type.hpp>
 #include <boost/serialization/list.hpp>
 #include <boost/multi_index_container.hpp>
 #include <boost/multi_index/global_fun.hpp>
