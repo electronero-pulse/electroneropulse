df131392916a090bc266aa854060934536d0cb77
⛓️INTERCHAINED ⚡Electronero Network ʕ•̫͡•ʔ-̫͡-ʕ•͓͡•ʔ-̫͡-ʕ•̫͡•ʔ-̫͡-ʔ <evansmark.work@gmail.com>
2025-06-12 22:00:59 -0400

    Update ubuntu_install.sh

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
-apt-get install build-essential cmake pkg-config libboost-all-dev libzmq3-dev libssl-dev libsodium-dev libunbound-dev libminiupnpc-dev libunwind8-dev liblzma-dev libreadline6-dev libldns-dev libexpat1-dev libgtest-dev doxygen graphviz
+apt-get update && \
+apt-get install --yes build-essential cmake pkg-config libboost-all-dev libzmq3-dev libssl-dev libsodium-dev libunbound-dev libminiupnpc-dev libunwind-dev libpcsclite-dev liblzma-dev libreadline-dev libldns-dev libexpat1-dev libgtest-dev doxygen graphviz
 echo "Dependencies installation complete"
+sleep 1s
 echo "Download Submodules"
+git submodule init && git submodule update
+sleep 1s
+echo "Make release"
+cd ~/electroneropulse && mkdir build && cd build && cmake .. && cd .. && make -j$(nproc)
 
